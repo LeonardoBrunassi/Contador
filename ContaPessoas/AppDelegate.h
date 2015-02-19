cb190e3ae4bdf09ccf01a11d465c266e7bffb94b
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-02-19 14:21:33 -0200

    Mudança de nome.

--- ContaPessoas/AppDelegate.h
+++ ContaPessoas/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Leonardo Brunassi on 2/18/15.
+//  Copyright (c) 2015 Leonardo Brunassi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
